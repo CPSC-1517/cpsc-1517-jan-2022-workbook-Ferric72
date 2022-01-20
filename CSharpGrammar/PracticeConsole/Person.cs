e4e7e8c56c957fd314654fe05efb76d076b9c101
Ferric72 <90295424+Ferric72@users.noreply.github.com>
2022-01-20 08:33:15 -0700

    Finished Person composite class

--- CSharpGrammar/PracticeConsole/Person.cs
+++ CSharpGrammar/PracticeConsole/Person.cs
@@ -9,6 +9,21 @@ namespace PracticeConsole.Data
     public class Person
     {
         //example of a composite class
+        //a composite class uses other classes in its definition
+        //a composite class is recognized with the phrase "has a" class
+        //this class of Person "has a" resident address
+
+        //in contrast, an inherited class extends another class in its
+        //  definition
+        //an inherited class is recognized with the phrase "is a" class
+        //  assume a general class called Transportation
+        //then we can "extend" this class to more specific classes
+        //      public class Vehicle : Transportation
+        //      public class Bike : Transportation
+        //      public class Boat : Transportation
+
+
+
         //each instance of this class will represent an individual
         //This class will define the following characteristics of a person
         //  First Name, Last Name, the current resident address, list of
@@ -43,15 +58,53 @@ namespace PracticeConsole.Data
             }
         }
 
+        //composite actually uses the other class as a property/field
+        //within the definition of the class being defined
+
         public ResidentAddress Address;
+
+        //composition
 
         public List<Employment> EmploymentPositions { get; set; }
 
-        public Person()
+       /* public Person()
         {
             //if an instance of List<T> is not created and assigned then
             //  the property will have an initial value of null
             EmploymentPositions = new List<Employment>();
+
+            //Option 1: assign some default value to the strings
+            //since FirstName and LastName need to have values
+            //you can assign default literals to the properties
+            FirstName = "unknown";
+            LastName = "unknown";
+        }*/
+
+        //Option 2:
+        //DO NOT code a "Default" constructor
+        //Code ONLY the "Greedy" constructor
+        //if only a greedy constructor exists for the class, the ONLY
+        //  way to possibly create an instance for the class within
+        //  the program is to use the greedy constructor when the class
+        //  instance is created
+
+        public Person(string firstname, string lastname,
+                        List<Employment> employmentpositions,
+                        ResidentAddress address)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            if (employmentpositions != null)
+            {
+                EmploymentPositions = employmentpositions;
+            }
+            else
+            {
+                //allows a null value and the class to have an
+                //  empty List<T>
+                EmploymentPositions = new List<Employment>();
+            }
+            Address = address;
         }
     }
 }
